af84e4027cb87c81904e9220760bb00235bf050c
Yan Titarenko <local.tourist.kiev@gmail.com>
2021-03-06 02:21:09 +0200

    Update controller.go

--- pkg/operator/ceph/nfs/controller.go
+++ pkg/operator/ceph/nfs/controller.go
@@ -19,7 +19,7 @@ package nfs
 import (
 	"context"
 	"fmt"
-	"reflect"
+	reflect2 "github.com/modern-go/reflect2"
 	"strings"
 
 	"github.com/coreos/pkg/capnslog"
@@ -58,7 +58,7 @@ var objectsToWatch = []client.Object{
 	&appsv1.Deployment{TypeMeta: metav1.TypeMeta{Kind: "Deployment", APIVersion: appsv1.SchemeGroupVersion.String()}},
 }
 
-var cephNFSKind = reflect.TypeOf(cephv1.CephNFS{}).Name()
+var cephNFSKind = reflect2.TypeOf(cephv1.CephNFS{}).Name()
 
 // Sets the type meta for the controller main object
 var controllerTypeMeta = metav1.TypeMeta{
